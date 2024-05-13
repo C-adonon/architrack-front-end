f8dc063aa7205dcb58a99fd00814caee450686d3
Chloé Adonon <99622570+C-adonon@users.noreply.github.com>
2024-05-13 09:41:59 +0200

    feat: :alembic: temporary conditional display for login

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <script setup>
+import { RouterView } from 'vue-router'
 import LoginView from './views/LoginView.vue';
+const authentified = true;
 </script>
 
 <template>
   <div>Architrack</div>
-  <LoginView />
+  <div v-if="!authentified">
+    <LoginView />
+  </div>
+  <div v-else>
+    <RouterView />
+  </div>
 </template>
 
 <style scoped></style>
