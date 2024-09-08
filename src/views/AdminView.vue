eaf89c43bfb5b7e02dc3383cf90f3507b4b64a1c
Chloé Adonon <99622570+C-adonon@users.noreply.github.com>
2024-09-08 03:46:38 +0200

    feat: :sparkles: initialized admin view

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,3 +1,20 @@
+<script setup>
+import SideBar from "@/components/nav/SideBar.vue";
+import { useAuthStore } from "@/store/authStore";
+</script>
+
 <template>
-    <div>This is for admins</div>
+  <main>
+    <SideBar />
+    <section class="container">
+      <div>This is for admins</div>
+      <div>{{ useAuthStore().user }}</div>
+    </section>
+  </main>
 </template>
+
+<style lang="scss" scoped>
+.container {
+  padding: 0 2vw;
+}
+</style>
