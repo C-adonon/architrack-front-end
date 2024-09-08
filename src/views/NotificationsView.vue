610fc7d8212fdcb1ad1cebec923434311c4b5cd8
Chloé Adonon <99622570+C-adonon@users.noreply.github.com>
2024-09-08 03:44:05 +0200

    feat: :sparkles: initialized notifications view

---
+++ src/views/NotificationsView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import SideBar from "@/components/nav/SideBar.vue";
+</script>
+<template>
+  <main>
+    <SideBar />
+    <section class="container">
+      <h1>Notifications</h1>
+      <div>This is for notifications</div>
+    </section>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+.container {
+   padding: 0 2vw;
+}
+</style>
