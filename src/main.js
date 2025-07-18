afbfa1d57e82ecbcf62bbb3e80801f4cfbfdc909
Chloé Adonon <99622570+C-adonon@users.noreply.github.com>
2025-07-18 23:22:55 +0200

    fix: :poop: adding history fallback middleware to fix reload error when using DO (test)

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import "primeicons/primeicons.css";
 import "primevue/resources/themes/aura-light-noir/theme.css";
 import "primeflex/primeflex.css";
 import ToastService from "primevue/toastservice";
+import historyApiFallback from "connect-history-api-fallback";
 
 const pinia = createPinia();
 const app = createApp(App);
@@ -16,5 +17,8 @@ app.use(pinia);
 app.use(router);
 app.use(PrimeVue);
 app.use(ToastService);
+
+// test history fallback
+app.use(historyApiFallback()); 
 
 app.mount("#app");
